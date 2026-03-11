baff434f2dafec5d188bbf3c80130c59f8c908ea
Mostafa Shora <mm7326745@gmail.com>
2026-03-11 17:24:02 +0200

    restructure project and move files to src

--- src/server.js
+++ src/server.js
@@ -1,6 +1,6 @@
 require('dotenv').config();
 const express = require('express');
-const connectDB = require('./config/db');
+const connectDB = require('./src/config/db');
 
 const app = express();
 app.use(express.json());
